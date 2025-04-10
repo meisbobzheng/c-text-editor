1af4cd09cd3b35e6e1ac52a2244cb2817f095b78
Bob Zheng <bob.zheng6042@gmail.com>
2025-04-09 22:45:57 -0400

    going to bed

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # kilo text editor, written in C
 
 from https://viewsourcecode.org/snaptoken/kilo
+
+current: ch 3, clear the screen
+
--- kilo.c
+++ kilo.c
@@ -1,51 +1,90 @@
-#include <unistd.h>
-#include <termios.h>
-#include <stdlib.h>
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
 
 struct termios orig_termios;
 
+/*** terminal ***/
+
 void die(const char *s) {
-    perror(s);
-    exit(1);
+  perror(s);
+  exit(1);
 }
 
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
 
-    struct termios raw = orig_termios;  
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_iflag &= ~(IXON | ICRNL);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= (CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 1;
+  atexit(disableRawMode);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_iflag &= ~(IXON | ICRNL);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
+}
+
+/*** output ***/
+void editorScreenRefresh() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
+/*** init ***/
+
 int main() {
-    enableRawMode();
+  enableRawMode();
 
-    while (1) {
-        char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+  while (1) {
+    editorScreenRefresh();
+    editorProcessKeypress();
+  }
 
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-
-        if (c == 'q') break;
-    }
-
-    return 0; 
+  return 0;
 }
